3becb0e6fa40917233a887a6f78ce5db6ca6b219
knee-rel <nirelmarieibarra@gmail.com>
2021-08-10 11:10:59 +0800

    fixed settigns

--- earp_backend/settings.py
+++ earp_backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q&7f3^7(l5ke+=n+wa-&b_=0=_&&url&-#fk95i^*&ygjb*^^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://earp-api.herokuapp.com', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL=True
 
 
